14e0741b75c1274d603ea7e60f12f977212ac7b4
Remington Brasga <Remington.Brasga@taec.toshiba.com>
2018-11-13 13:46:01 -0800

    Cleaned up repository, removed redundant files

---
+++ software/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -specs=Xilinx.spec -Wl,--build-id=none -Wl,--start-group,-lgcc,-lxil,-lc,--end-group
+
