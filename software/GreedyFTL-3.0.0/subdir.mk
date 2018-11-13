f9c3941e5c10db61cb0979eef8c2f5b625c6bd4a
Remington Brasga <Remington.Brasga@taec.toshiba.com>
2018-11-13 14:00:59 -0800

    Removed pre-compiled .o files Updated make clean to remove .o files Added subdir.mk to each GreedyFTL folder

---
+++ software/GreedyFTL-3.0.0/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/fmc_driver.c \
+../src/init_ftl.c \
+../src/low_level_scheduler.c \
+../src/lru_buffer.c \
+../src/main.c \
+../src/page_map.c 
+
+OBJS += \
+./src/fmc_driver.o \
+./src/init_ftl.o \
+./src/low_level_scheduler.o \
+./src/lru_buffer.o \
+./src/main.o \
+./src/page_map.o 
+
+C_DEPS += \
+./src/fmc_driver.d \
+./src/init_ftl.d \
+./src/low_level_scheduler.d \
+./src/lru_buffer.d \
+./src/main.d \
+./src/page_map.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM gcc compiler'
+	arm-eabi-gcc -mcpu=cortex-a9 -mfloat-abi=soft -Wall -O2 -c -fmessage-length=0 -MT"$@" -I../../BSP/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
